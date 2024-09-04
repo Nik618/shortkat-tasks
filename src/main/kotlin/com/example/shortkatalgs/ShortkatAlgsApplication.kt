e67c8b39c3e74d90e24efe6367478672b9d354f1
trinax <trinax01@gmail.com>
2024-09-04 20:28:51 +0300

    init

---
+++ src/main/kotlin/com/example/shortkatalgs/ShortkatAlgsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.shortkatalgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortkatAlgsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShortkatAlgsApplication>(*args)
+}
