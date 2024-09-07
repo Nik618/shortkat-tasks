3c8de01f2101be9394bf8a23fbb089845cf0fc4c
trinax <trinax01@gmail.com>
2024-09-07 06:41:17 +0300

    change language to java

---
+++ src/main/java/com/example/shortkatalgs/easy/ArrayTasksJava.java
@@ -0,0 +1,30 @@
+package com.example.shortkatalgs.easy;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ArrayTasksJava {
+
+    public void removeDuplicates() {
+        Integer[] nums = {1, 5, 8, 8, 12, 34, 37, 37, 37, 68, 89, 89};
+
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 == nums.length) {
+                arrayList.add(nums[i]);
+                break;
+            }
+            if (!nums[i].equals(nums[i + 1])) {
+                arrayList.add(nums[i]);
+            }
+
+        }
+        int k = arrayList.size();
+        nums = arrayList.toArray(new Integer[0]);
+
+        System.out.println(Arrays.stream(nums).toList() + " " + k);
+    }
+
+}
--- src/main/kotlin/com/example/shortkatalgs/Processor.kt
+++ src/main/kotlin/com/example/shortkatalgs/Processor.kt
@@ -1,17 +1,18 @@
 package com.example.shortkatalgs
 
 import com.example.shortkatalgs.easy.ArrayTasks
+import com.example.shortkatalgs.easy.ArrayTasksJava
 import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Service
 
 @Service
 class Processor(
-    private val arrayTasks: ArrayTasks
+    private val arrayTasksJava: ArrayTasksJava
 ) {
 
     @PostConstruct
     private fun startTasks() {
-        arrayTasks.removeDuplicates()
+        arrayTasksJava.removeDuplicates()
     }
 
 }
